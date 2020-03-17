4ef00299e799135371589f2f24f248b5f05a4b4c
Adam Hoyle <atomoil@gmail.com>
2020-03-17 17:57:27 +0000

    improve error handling

--- main.js
+++ main.js
@@ -132,19 +132,23 @@ async function uploadFiles(files, bucketName) {
     const source = path.resolve(filepath)
     const destination = destinationFolder + '/' + path.relative(".", filepath)
     console.log(`GC > uploading ${source} to ${destination}`)
-    await storage.bucket(bucketName).upload(source, {
-      // Support for HTTP requests made with `Accept-Encoding: gzip`
-      gzip: true,
-      // By setting the option `destination`, you can change the name of the
-      // object you are uploading to a bucket.
-      destination: destination,
-      metadata: {
-        // Enable long-lived HTTP caching headers
-        // Use only if the contents of the file will never change
-        // (If the contents will change, use cacheControl: 'no-cache')
-        cacheControl: 'public, max-age=31536000',
-      },
-    })
+    try {
+      await storage.bucket(bucketName).upload(source, {
+        // Support for HTTP requests made with `Accept-Encoding: gzip`
+        gzip: true,
+        // By setting the option `destination`, you can change the name of the
+        // object you are uploading to a bucket.
+        destination: destination,
+        metadata: {
+          // Enable long-lived HTTP caching headers
+          // Use only if the contents of the file will never change
+          // (If the contents will change, use cacheControl: 'no-cache')
+          cacheControl: 'public, max-age=31536000',
+        },
+      })
+    } catch (err) {
+      console.log(`Error uploading ${source} > ${err}`)
+    }
   }
 }
 
